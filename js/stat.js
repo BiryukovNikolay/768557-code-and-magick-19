34ea6cc8350250a59d18823c4cdde46500f64aa0
Biryukov-Nikolay <jackson_klyazma@mail.ru>
2020-01-29 20:30:51 +0300

    correction funktion

--- js/stat.js
+++ js/stat.js
@@ -1,42 +1,42 @@
 'use strict';
 
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var CLOUD_X_1 = 300;
-var CLOUD_Y_1 = 30;
-var CLOUD_X_2 = 600;
-var CLOUD_Y_2 = 20;
-var CLOUD_X_3 = 550;
-var CLOUD_Y_3 = 120;
-var CLOUD_X_4 = 600;
-var CLOUD_Y_4 = 270;
-var CLOUD_X_5 = 250;
-var CLOUD_Y_5 = 250;
-var CLOUD_X_6 = 50;
-var CLOUD_Y_6 = 280;
-var CLOUD_X_7 = 80;
-var CLOUD_Y_7 = 180;
-var topLine = [CLOUD_Y, CLOUD_Y_1, CLOUD_Y_2];
-var bottomLine = [CLOUD_Y_4, CLOUD_Y_5, CLOUD_Y_6];
-var leftLine = [CLOUD_X, CLOUD_X_7, CLOUD_X_6];
-var rightLine = [CLOUD_X_2, CLOUD_X_3, CLOUD_X_4];
-var GAP = 10;
-var INDENT = 30;
-var TIME_LABEL = 20;
-var SPACE_COLUMN = 50;
+var X = 100;
+var Y = 10;
+var X_1 = 300;
+var Y_1 = 30;
+var X_2 = 600;
+var Y_2 = 20;
+var X_3 = 550;
+var Y_3 = 120;
+var X_4 = 600;
+var Y_4 = 270;
+var X_5 = 250;
+var Y_5 = 250;
+var X_6 = 50;
+var Y_6 = 280;
+var X_7 = 80;
+var Y_7 = 180;
+var topLine = [Y, Y_1, Y_2];
+var bottomLine = [Y_4, Y_5, Y_6];
+var leftLine = [X, X_7, X_6];
+var rightLine = [X_2, X_3, X_4];
+var GAP = 10; // Размер отступа тени
+var INDENT = 30; // Размер отступа блока статистики от кривых (вписанный прямоугольник)
+var TIME_LABEL = 20; // Размер отступа показателя времени от столбцов
+var SPACE_COLUMN = 50; // Размер отступа между столбцами
 
 
-var renderCloud = function (ctx, x, y, X_1, Y_1, X_2, Y_2, X_3, Y_3, X_4, Y_4, X_5, Y_5, X_6, Y_6, X_7, Y_7, color) {
+var renderCloud = function (ctx, interval, color) {
   ctx.fillStyle = color;
   ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(X_1, Y_1);
-  ctx.lineTo(X_2, Y_2);
-  ctx.lineTo(X_3, Y_3);
-  ctx.lineTo(X_4, Y_4);
-  ctx.lineTo(X_5, Y_5);
-  ctx.lineTo(X_6, Y_6);
-  ctx.lineTo(X_7, Y_7);
+  ctx.moveTo(X + interval, Y + interval);
+  ctx.lineTo(X_1 + interval, Y_1 + interval);
+  ctx.lineTo(X_2 + interval, Y_2 + interval);
+  ctx.lineTo(X_3 + interval, Y_3 + interval);
+  ctx.lineTo(X_4 + interval, Y_4 + interval);
+  ctx.lineTo(X_5 + interval, Y_5 + interval);
+  ctx.lineTo(X_6 + interval, Y_6 + interval);
+  ctx.lineTo(X_7 + interval, Y_7 + interval);
   ctx.closePath();
   ctx.fill();
 };
@@ -61,21 +61,24 @@ var getMinElement = function (arr) {
   return minElement;
 };
 
+// Определяем границы блока статистики (прямоугольника вписанного внутрь многоугольника)
 var topPoint = getMaxElement(topLine) + INDENT;
 var bottomPoint = getMinElement(bottomLine) - INDENT;
 var leftPoint = getMaxElement(leftLine) + INDENT;
 var rightPoint = getMinElement(rightLine) - INDENT;
+// Параметры блока статистики
 var maxHeight = bottomPoint - topPoint;
 var maxWidth = rightPoint - leftPoint;
+// Параметры столбцов
 var maxHeightColumn = (maxHeight - 3 * GAP) - TIME_LABEL;
 var widthColumn = maxWidth / 4 - SPACE_COLUMN;
 
 // Отрисовка облака статистики
 window.renderStatistics = function (ctx, players, times) {
   // Отрисовка тени облака (координаты +GAP относительно поля статистики)
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_X_1 + GAP, CLOUD_Y_1 + GAP, CLOUD_X_2 + GAP, CLOUD_Y_2 + GAP, CLOUD_X_3 + GAP, CLOUD_Y_3 + GAP, CLOUD_X_4 + GAP, CLOUD_Y_4 + GAP, CLOUD_X_5 + GAP, CLOUD_Y_5 + GAP, CLOUD_X_6 + GAP, CLOUD_Y_6 + GAP, CLOUD_X_7 + GAP, CLOUD_Y_7 + GAP, 'rgba(0, 0, 0, 0.3)');
-  // Отривовка поля статистики
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_X_1, CLOUD_Y_1, CLOUD_X_2, CLOUD_Y_2, CLOUD_X_3, CLOUD_Y_3, CLOUD_X_4, CLOUD_Y_4, CLOUD_X_5, CLOUD_Y_5, CLOUD_X_6, CLOUD_Y_6, CLOUD_X_7, CLOUD_Y_7, '#fff');
+  renderCloud(ctx, GAP, 'rgba(0, 0, 0, 0.3)');
+  // Отривовка облака статистики
+  renderCloud(ctx, 0, '#fff');
   ctx.stroke();
 
   ctx.fillStyle = '#000';
